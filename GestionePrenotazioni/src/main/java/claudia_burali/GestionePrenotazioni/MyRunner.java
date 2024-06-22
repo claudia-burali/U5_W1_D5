d283987b0a6b3ca049f51be643f921f9fd9742f2
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-06-22 03:11:36 +0200

    creazione postazioni e prenotazioni

--- GestionePrenotazioni/src/main/java/claudia_burali/GestionePrenotazioni/MyRunner.java
+++ GestionePrenotazioni/src/main/java/claudia_burali/GestionePrenotazioni/MyRunner.java
@@ -2,14 +2,18 @@ package claudia_burali.GestionePrenotazioni;
 
 import claudia_burali.GestionePrenotazioni.entities.Edificio;
 import claudia_burali.GestionePrenotazioni.entities.Postazione;
+import claudia_burali.GestionePrenotazioni.entities.Prenotazione;
 import claudia_burali.GestionePrenotazioni.entities.Utente;
 import claudia_burali.GestionePrenotazioni.enums.TipoPostazione;
 import claudia_burali.GestionePrenotazioni.services.EdificioService;
 import claudia_burali.GestionePrenotazioni.services.PostazioneService;
+import claudia_burali.GestionePrenotazioni.services.PrenotazioneService;
 import claudia_burali.GestionePrenotazioni.services.UtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class MyRunner implements CommandLineRunner {
@@ -19,6 +23,8 @@ public class MyRunner implements CommandLineRunner {
     private EdificioService edificioService;
     @Autowired
     private PostazioneService postazioneService;
+    @Autowired
+    private PrenotazioneService prenotazioneService;
     @Override
     public void run(String... args) throws Exception {
         System.out.println("runner");
@@ -26,17 +32,17 @@ public class MyRunner implements CommandLineRunner {
         Utente utente2 = new Utente("Gianni99", "Giovanni Verdi", "giovanni.verdi@epicode.com");
         Utente utente3 = new Utente("Fra89", "Francesca Bianchi", "francesca.bianchi@epicode.com");
 
-        //utenteService.saveUtente(utente1);
-        //utenteService.saveUtente(utente2);
-        //utenteService.saveUtente(utente3);
+        utenteService.saveUtente(utente1);
+        utenteService.saveUtente(utente2);
+        utenteService.saveUtente(utente3);
 
         Edificio edificio1 = new Edificio("Open Studio", "via Roma, 10", "Milano");
         Edificio edificio2 = new Edificio("Uffici Generali", "piazza Dante, 2", "Roma");
         Edificio edificio3 = new Edificio("Casa Ghianda", "viale Liberazione, 123", "Firenze");
 
-        //edificioService.saveEdificio(edificio1);
-        //edificioService.saveEdificio(edificio2);
-        //edificioService.saveEdificio(edificio3);
+        edificioService.saveEdificio(edificio1);
+        edificioService.saveEdificio(edificio2);
+        edificioService.saveEdificio(edificio3);
 
         Postazione postazione1 = new Postazione("ufficio luminoso", TipoPostazione.OPENSPACE, 42, edificio3 );
         Postazione postazione2 = new Postazione("sala silenziosa", TipoPostazione.SALA_RIUNIONI, 15, edificio3 );
@@ -49,6 +55,18 @@ public class MyRunner implements CommandLineRunner {
         postazioneService.savePostazione(postazione3);
         postazioneService.savePostazione(postazione4);
         postazioneService.savePostazione(postazione5);
+
+        Prenotazione prenotazione1 = new Prenotazione(postazione1, utente1, LocalDate.of(2024, 7, 12));
+        Prenotazione prenotazione2 = new Prenotazione(postazione1, utente3, LocalDate.of(2024, 6, 30));
+        Prenotazione prenotazione3 = new Prenotazione(postazione2, utente2, LocalDate.of(2024, 7, 3));
+        Prenotazione prenotazione4 = new Prenotazione(postazione3, utente1, LocalDate.of(2024, 8, 1));
+        Prenotazione prenotazione5 = new Prenotazione(postazione2, utente3, LocalDate.of(2024, 7, 22));
+
+        prenotazioneService.savePrenotazione(prenotazione1);
+        prenotazioneService.savePrenotazione(prenotazione2);
+        prenotazioneService.savePrenotazione(prenotazione3);
+        prenotazioneService.savePrenotazione(prenotazione4);
+        prenotazioneService.savePrenotazione(prenotazione5);
 
     }
 }
--- GestionePrenotazioni/src/main/java/claudia_burali/GestionePrenotazioni/services/PrenotazioneService.java
+++ GestionePrenotazioni/src/main/java/claudia_burali/GestionePrenotazioni/services/PrenotazioneService.java
@@ -1,5 +1,6 @@
 package claudia_burali.GestionePrenotazioni.services;
 
+import claudia_burali.GestionePrenotazioni.entities.Prenotazione;
 import claudia_burali.GestionePrenotazioni.repositories.PrenotazioneRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -7,5 +8,11 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class PrenotazioneService {
-    private PrenotazioneRepository prenotazioneRepository;
+    private final PrenotazioneRepository prenotazioneRepository;
+    public PrenotazioneService(PrenotazioneRepository prenotazioneRepository) {
+        this.prenotazioneRepository = prenotazioneRepository;
+    }
+    public void savePrenotazione(Prenotazione prenotazione) {
+        prenotazioneRepository.save(prenotazione);
+    }
 }
